a92fbf1b0d8d2e7e75aee0ff22167408ae750318
Zachary Gannon <zachgannon93@gmail.com>
2017-10-19 10:03:31 -0400

    [WannaCookie] Initial commit.

---
+++ wannacookie/wannacookie.py
@@ -0,0 +1,32 @@
+"""What, do you want a cookie?"""
+
+import discord
+from discord.ext import commands
+
+
+class WannaCookie:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='wanna', pass_context=True, no_pm=True)
+    async def wanna(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("Type help wanna for info.")
+
+    @wanna.command(name='cookie', pass_context=True, no_pm=True)
+    async def cookie(self, ctx, user : discord.Member=None):
+        msg = 'What, do you want a cookie {}?'
+        if user is not None:
+            if user.id == self.bot.user.id:
+                user = 'Me'
+                await self.bot.say(msg.format(user) + "\n\nERROR: No, no I do not want a cookie, I am a bot and am not capable of eating cookies.")
+            else:
+                await self.bot.say(msg.format(user.mention))
+        else:
+            user = ctx.message.author
+            await self.bot.say(msg.format(user.mention))
+
+
+def setup(bot):
+    bot.add_cog(WannaCookie(bot))
