900e0607d84d4a7254828c1caeb4c4161ce19d1d
Zachary Gannon <zachgannon93@gmail.com>
2017-10-26 12:30:34 -0400

    [WannaCookie] Fixed (hopefully) refactoring error (needs testing).

--- wannacookie/wannacookie.py
+++ wannacookie/wannacookie.py
@@ -13,16 +13,35 @@ class WannaCookie:
     async def _wanna(self, context):
         """What, do you want a..."""
         if context.invoked_subcommand is None:
-            await self.bot.say("Type `[p]help wanna` for info.")
+            await self.bot.say('Type `[p]help wanna` for info.')
 
-    @wanna.command(name='cookie', pass_context=True)
+    @_wanna.command(name='cookie', pass_context=True)
     async def cookie(self, context, user: discord.Member=None):
         """What, do you want a cookie?"""
         msg = 'What, do you want a cookie {}?'
         if user is not None:
             if user.id == self.bot.user.id:
                 user = '... Me??'
-                await self.bot.say(msg.format(user) + "\n\nERROR: No, no I do not want a cookie, I am a bot and am not capable of eating cookies.")
+                await self.bot.say(msg.format(user) + (
+                    '\n\nERROR: No, no I do not want a cookie, '
+                    'I am a bot and am not capable of eating cookies.'))
+            else:
+                await self.bot.say(msg.format(user.mention))
+        else:
+            user = context.message.author
+            await self.bot.say(msg.format(user.mention))
+
+    @_wanna.command(name='goldstar', pass_context=True)
+    async def goldstar(self, context, user: discord.Member=None):
+        """What, do you want a gold star?"""
+        msg = 'What, do you want a gold star {}?'
+        if user is not None:
+            if user.id == self.bot.user.id:
+                user = '... Me??'
+                await self.bot.say(msg.format(user) + (
+                    '\n\nERROR: No, no I do not want a gold star, '
+                    'I am a bot and have not material desires such '
+                    'as you filthy humans.'))
             else:
                 await self.bot.say(msg.format(user.mention))
         else:
